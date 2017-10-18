c5fee1afad8402aa886fbaac4c689506934aaba8
Egidijus Zemgulys <egidijuszemgulys@hotmail.com>
2017-10-18 13:45:10 +0100

    Add PatronController and corresponding views and create PatronSevice

---
+++ LibraryService/PatronService.cs
@@ -0,0 +1,70 @@
+﻿using LibraryData;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using LibraryData.Models;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryService
+{
+    public class PatronService : IPatron
+    {
+        private LibraryContext _context;
+
+        public PatronService(LibraryContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(Patron newPatron)
+        {
+            _context.Add(newPatron);
+            _context.SaveChanges();
+        }
+
+        public IEnumerable<Patron> GetAll()
+        {
+            return _context.Patrons
+                .Include(p => p.LibraryCard)
+                .Include(p => p.HomeLibraryBranch);
+        }
+
+        public Patron GetById(int id)
+        {
+            return GetAll().FirstOrDefault(p => p.Id == id);
+        }
+
+        public IEnumerable<CheckoutHistory> GetCheckoutHistory(int patronId)
+        {
+            var cardId = GetById(patronId).LibraryCard.Id;
+              
+            return _context.CheckoutHistories
+                .Include(ch => ch.LibraryCard)
+                .Include(ch => ch.LibraryAsset)
+                .Where(ch => ch.LibraryCard.Id == cardId)
+                .OrderByDescending(ch => ch.CheckedOut);
+        }
+
+        public IEnumerable<Checkout> GetCheckouts(int patronId)
+        {
+            var cardId = GetById(patronId).LibraryCard.Id;
+
+            return _context.Checkouts
+                .Include(ch => ch.LibraryCard)
+                .Include(ch => ch.LibraryAsset)
+                .Where(c => c.LibraryCard.Id == cardId);
+        }
+
+        public IEnumerable<Hold> GetHolds(int patronId)
+        {
+            var cardId = GetById(patronId).LibraryCard.Id;
+
+            return _context.Holds
+                .Include(h => h.LibraryCard)
+                .Include(h => h.LibraryAsset)
+                .Where(h => h.LibraryCard.Id == cardId)
+                .OrderByDescending(h => h.HoldPlaced);
+        }
+    }
+}
